b65840e6d3d36098d7a30d804326fc24cb7b28c2
Gabriel Gambarra <gabriel.gambarra@itriad.org.br>
2019-09-25 14:43:10 -0400

    fix mongo connection

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect('mongodb://localhost/semana-omnistack', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/semana-omnistack7');
 //connect //createConnection
 mongoose.Promise = global.Promise;
 
