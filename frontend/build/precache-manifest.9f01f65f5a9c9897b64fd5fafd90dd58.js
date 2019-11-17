07bb6f5bd4b0a42dfb032cb521dfd887d23964c1
Gabriel Gambarra <gambarragabriel@gmail.com>
2019-11-17 00:05:50 -0400

    Deploy front

---
+++ frontend/build/precache-manifest.9f01f65f5a9c9897b64fd5fafd90dd58.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22dfc1e71fd41cac329bac17817c7832",
+    "url": "/instarocket/index.html"
+  },
+  {
+    "revision": "dabbc763b9aed38eefa0",
+    "url": "/instarocket/static/css/main.4751c969.chunk.css"
+  },
+  {
+    "revision": "c7bbebea98e338159796",
+    "url": "/instarocket/static/js/2.a42715b0.chunk.js"
+  },
+  {
+    "revision": "dabbc763b9aed38eefa0",
+    "url": "/instarocket/static/js/main.421e7607.chunk.js"
+  },
+  {
+    "revision": "e3437ae96bed0e85fe73",
+    "url": "/instarocket/static/js/runtime~main.efd837b8.js"
+  },
+  {
+    "revision": "787c6af98ac770e43e65815a8ff337df",
+    "url": "/instarocket/static/media/camera.787c6af9.svg"
+  },
+  {
+    "revision": "e2a499354e3b1d309ff71584d7290d29",
+    "url": "/instarocket/static/media/comment.e2a49935.svg"
+  },
+  {
+    "revision": "00e13584091199bddd3785adaf0b9f3d",
+    "url": "/instarocket/static/media/like.00e13584.svg"
+  },
+  {
+    "revision": "83fd0bcc6cdc21e671ae5edf856ba3ef",
+    "url": "/instarocket/static/media/logo.83fd0bcc.svg"
+  },
+  {
+    "revision": "22d960a9ac06cc252579791795bdea8c",
+    "url": "/instarocket/static/media/more.22d960a9.svg"
+  },
+  {
+    "revision": "55e74cfdc1470a74c5bc8c2af6ed9fa0",
+    "url": "/instarocket/static/media/send.55e74cfd.svg"
+  }
+]);
